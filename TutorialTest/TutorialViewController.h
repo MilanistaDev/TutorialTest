008797e6257682e40032631276023441ed1cb008
MilanistaDev <milanista224@gmail.com>
2014-10-23 17:31:47 +0900

    Upload

---
+++ TutorialTest/TutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialViewController.h
+//  TutorialTest
+//
+//  Created by 麻生 拓弥 on 2014/10/23.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialViewController : UIViewController
+
+@end
---
+++ TutorialTest/TutorialViewController.m
@@ -0,0 +1,37 @@
+//
+//  TutorialViewController.m
+//  TutorialTest
+//
+//  Created by 麻生 拓弥 on 2014/10/23.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+#import "TutorialViewController.h"
+
+@interface TutorialViewController ()
+
+@end
+
+@implementation TutorialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
