c6f3a888e60b7d3513277d515db8900fd896e0e3
MilanistaDev <milanista224@gmail.com>
2014-10-23 16:36:16 +0900

    Initial Commit

---
+++ TutorialTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TutorialTest
+//
+//  Created by 麻生 拓弥 on 2014/10/23.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
